a5181e0cefe01bacbaa4f937aaa714e263fc756b
hkss22 <bergaminhohkss@gmail.com>
2024-05-10 03:16:44 -0300

    Corrigindo configurações de banco de dados2

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
-import mysql from 'mysql';
+import mysql2 from 'mysql2';
 
 // Conexao com o banco
-const db = mysql.createPool({
+const db = mysql2.createPool({
     host: "viaduct.proxy.rlwy.net",
     user: "root",
     password: "pdABqKSTcEmYNKjxSxhYsSsljScBKNhj",
