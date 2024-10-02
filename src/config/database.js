1d17b8815ce417c32b62df313b5b45064e4159ae
hkss22 <103163674+hkss22@users.noreply.github.com>
2024-10-02 10:42:17 -0300

    Update database.js
    Configuração de conexão do novo database

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,12 @@ import mysql2 from 'mysql2';
 
 // Conexao com o banco
 const db = mysql2.createPool({
-    host: "monorail.proxy.rlwy.net",
+    host: "junction.proxy.rlwy.net",
     user: "root",
     password: "oKNyyamtumlmRHleYOrUyOAebQyfAySz",
     database: "railway",
     connectTimeout: 30000,
-    port: '27241'
+    port: '26048'
 });
 
 
